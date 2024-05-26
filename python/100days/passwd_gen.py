a85cf7e7fd71bd8a47be262850bfa20b94f81766
mnvndraah <mnvndraahdev@gmail.com>
2024-05-26 12:49:48 +0530

    Update passwd_gen.py

--- python/100days/passwd_gen.py
+++ python/100days/passwd_gen.py
@@ -1,6 +1,7 @@
 from string import ascii_letters
 import random
 
+print("Welcome to the PyPassword Generator")
 letters = list(ascii_letters)
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
@@ -9,15 +10,21 @@ letter = int(input("How many characters would you like?\n"))
 number = int(input("How many numbers would you like?\n"))
 symbol = int(input("How many symbols would you like?\n"))
 
+passwd_list = []
 passwd = ""
 
 for i in range(letter):
-    passwd += random.choice(letters)
+    passwd_list += random.choice(letters)
 
 for j in range(number):
-    passwd += random.choice(letters)
+    passwd_list += random.choice(numbers)
 
 for k in range(symbol):
-    passwd += random.choice(symbols)
+    passwd_list += random.choice(symbols)
 
-print(passwd)
+random.shuffle(passwd_list)
+
+for _ in passwd_list:
+    passwd += _
+
+print(f"Your password is {passwd}")
