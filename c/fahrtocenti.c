5cce711a0e3ea578c5d81de989c57eec2c5695f6
mnvndraah <mnvndraahdev@gmail.com>
2024-05-28 06:50:23 +0530

    C commit

---
+++ c/fahrtocenti.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// Centigrade = (5/9)(Farenheit - 32)
+
+int main()
+{
+    int fahr, centi;
+    int low, up, step;
+    
+    low = 0;
+    up = 300;
+    step = 20;
+
+    fahr = low;
+    while (fahr<=up){
+        centi = (fahr-32) * 5/9;
+        printf("%d\t%d\n", fahr, centi);
+        fahr = fahr + step;
+    }
+}
