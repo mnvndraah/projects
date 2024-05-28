427b13ef9e81246b865bb23a886102e4a0d7aac3
mnvndraah <mnvndraahdev@gmail.com>
2024-05-28 08:08:17 +0530

    Updated farhtocenti.c

--- c/fahrtocenti.c
+++ c/fahrtocenti.c
@@ -9,11 +9,12 @@ int main()
     low = 0;
     up = 300;
     step = 20;
-
     fahr = low;
+    
     while (fahr<=up){
         centi = (fahr-32) * 5/9;
-        printf("%d\t%d\n", fahr, centi);
+        // printf("%d\t%d\n", fahr, centi); NOT RIGHT ORIENTED
+        printf("%3d %6d\n", fahr, centi); // RIGHT ORIENTED
         fahr = fahr + step;
     }
 }
