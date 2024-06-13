e9e3fe79aa8905f356aee9102ed2c45d2653aed9
mnvndraah <mnvndraahdev@gmail.com>
2024-06-13 10:12:42 +0530

    Add 1_lesson.c

---
+++ c/1_lesson.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char myName[] = "mnvndraah";
+    int myAge = 16;
+    // float myScore = 93.6;
+
+    printf("Hi, I am %s and my age is %d.\n", myName, myAge);
+    
+    
+
+   return 0; 
+}
